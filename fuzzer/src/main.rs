06517b5ed3e111431eea22ab78cb1d94bf11acb7
Bastian Köcher <info@kchr.de>
2024-05-07 10:35:42 +0200

    Also fuzz the metadata

--- fuzzer/src/main.rs
+++ fuzzer/src/main.rs
@@ -5,31 +5,16 @@ use merkleized_metadata::{generate_proof_for_extrinsic, verify_proof};
 use std::fs;
 
 fn main() {
-    let metadata = String::from_utf8(
-        fs::read(format!(
-            "{}/../fixtures/rococo_metadata_v15",
-            env!("CARGO_MANIFEST_DIR")
-        ))
-        .unwrap(),
-    )
-    .unwrap();
-
-    let metadata = Option::<Vec<u8>>::decode(
-        &mut &array_bytes::hex2bytes(metadata.strip_suffix("\n").unwrap()).unwrap()[..],
-    )
-    .unwrap()
-    .unwrap();
-
-    let metadata = RuntimeMetadataPrefixed::decode(&mut &metadata[..])
-        .unwrap()
-        .1;
-
     loop {
-        fuzz!(|data: (&[u8], Option<Vec<u8>>)| {
-            let (extrinsic, additional_signed) = data;
+        fuzz!(|data: (&[u8], &[u8], Option<Vec<u8>>)| {
+            let (metadata, extrinsic, additional_signed) = data;
+
+            let Ok(metadata) = RuntimeMetadataPrefixed::decode(&mut &metadata[..]) else {
+                return;
+            };
 
             let Ok(proof) =
-                generate_proof_for_extrinsic(extrinsic, additional_signed.as_deref(), &metadata)
+                generate_proof_for_extrinsic(extrinsic, additional_signed.as_deref(), &metadata.1)
             else {
                 // Invalid `extrinsic`, let's skip it.
                 return;
@@ -37,7 +22,7 @@ fn main() {
 
             println!("Generated valid extrinsic");
 
-            verify_proof(extrinsic, additional_signed.as_deref(), &metadata, &proof).unwrap()
+            verify_proof(extrinsic, additional_signed.as_deref(), &metadata.1, &proof).unwrap()
         });
     }
 }
